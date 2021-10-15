501015903c562a6394e833b212899616ea7e1363
Junhyuck Ko <mrbartrns@naver.com>
2021-10-15 17:18:16 +0900

    Fix bug not raising error when logging in failed

--- src/services/setupInterceptors.js
+++ src/services/setupInterceptors.js
@@ -2,6 +2,9 @@ import axiosInstance from "./api";
 import TokenService from "./token.service";
 import { refreshToken } from "../actions/auth";
 
+/**
+ * request, response를 받을때 error를 정의하는 interceptor
+ */
 const setup = (store) => {
   axiosInstance.interceptors.request.use(
     (config) => {
@@ -23,14 +26,13 @@ const setup = (store) => {
     },
     async (err) => {
       const originalConfig = err.config;
-
-      if (!TokenService.getLocalRefreshToken()) {
-        return;
-      }
-
-      if (originalConfig.url !== "user/login" && err.response) {
+      if (originalConfig.url !== "/user/login" && err.response) {
         // Access token was expired
         // TODO: modify logic when refresh token === null
+
+        if (!TokenService.getLocalRefreshToken()) {
+          return;
+        }
         if (err.response.status === 401 && !originalConfig._retry) {
           originalConfig._retry = true;
 
