a539bf944794e06ea2dc40c5b9260ea17b7efed8
Junhyuck Ko <mrbartrns@naver.com>
2021-10-20 15:27:48 +0900

    Separate form from comment component

--- src/components/Comment/comment.component.js
+++ src/components/Comment/comment.component.js
@@ -4,48 +4,16 @@ import { connect } from "react-redux";
 
 // custom imports
 import CommentTemplate from "../../templates/comment.template";
+import Comment from "./form.component";
 
 // css
 import "../../css/component/comment.component.scss";
 
 // TODO: CommentPagination 만들기
-function Comment(props) {
-  return (
-    <div className="comment">
-      <div className="comment__info">
-        <span className="comment__info__username">
-          {props.comment.author.username}
-        </span>
-        <span className="comment__info__dt_created">
-          {new Date(props.comment.dt_created).toLocaleString()}
-        </span>
-      </div>
-      <div className="comment__content">
-        {new Date(props.comment.dt_created).valueOf() + 1000 <
-          new Date(props.comment.dt_modified).valueOf() && (
-          <span className="modified">**수정됨</span>
-        )}
-        <span className="content">{props.comment.content}</span>
-      </div>
-      {!props.comment.parent && (
-        <button
-          className="comment__child_toggle_btn"
-          onClick={props.onClickFunction}
-        >
-          답글{" "}
-          <span className="comment__child_count">
-            {props.comment.children_count}
-          </span>
-        </button>
-      )}
-    </div>
-  );
-}
 
 function CommentComponent(props) {
   return (
     <div className="comment-container">
-      {/* TODO: separate form from CommentComponent and create new Component */}
       {/** Form Component */}
       {props.isLoggedIn && (
         <form
