871a7f55fb14fdb66b19f12cafc88514a553e301
Junhyuck Ko <mrbartrns@naver.com>
2021-11-04 15:51:31 +0900

    Design like component on detail

--- src/components/Detail/detail.component.js
+++ src/components/Detail/detail.component.js
@@ -6,18 +6,32 @@ import React from "react";
 // TODO: ListComponent처럼 detail componenet 나누기 -> CSS 처리 위함
 function DetailComponent(props) {
   return props.loaded ? (
-    <ul>
-      <li>이름: {props.post.name_kor}</li>
-      <li>영문 이름: {props.post.name}</li>
-      <li>이동 속도: {props.post.speed}</li>
-      <li>
-        <img
-          src={props.post.images.length > 0 ? props.post.images[0].image : null}
-          alt={props.post.name}
-        />
-      </li>
-      <li>공포 범위: {props.post.terror_radius}m</li>
-    </ul>
+    <div className="content_box">
+      <ul className="content">
+        <li>이름: {props.post.name_kor}</li>
+        <li>영문 이름: {props.post.name}</li>
+        <li>이동 속도: {props.post.speed}</li>
+        <li>
+          <img
+            src={
+              props.post.images.length > 0 ? props.post.images[0].image : null
+            }
+            alt={props.post.name}
+          />
+        </li>
+        <li>공포 범위: {props.post.terror_radius}m</li>
+      </ul>
+      <div className="like_box_container">
+        <div className="like_box__like like_box">
+          <span>좋아요</span>
+          <span>{props.post.like_count}</span>
+        </div>
+        <div className="like_box__dislike like_box">
+          <span>싫어요</span>
+          <span>{props.post.dislike_count}</span>
+        </div>
+      </div>
+    </div>
   ) : (
     <ul></ul>
   );
--- src/templates/detail.template.js
+++ src/templates/detail.template.js
@@ -8,19 +8,29 @@ import UserService from "../services/user.service";
 import DetailComponent from "../components/Detail/detail.component";
 import CommentTemplate from "./comment.template";
 
+// css
+import "../css/component/detail.component.scss";
+
 function DetailTemplate(props) {
   const location = useLocation();
   const [article, setArticle] = useState({});
   const [loaded, setLoaded] = useState(false);
   useEffect(() => {
-    setLoaded(false);
-    UserService.getApiDetail(location.pathname).then((response) => {
-      setArticle(response.data);
-      setLoaded(true);
-    });
+    let mounted = true;
+    if (mounted) {
+      setLoaded(false);
+      UserService.getApiDetail(location.pathname).then((response) => {
+        setArticle(response.data);
+        setLoaded(true);
+        console.log(response.data);
+      });
+    }
+    return () => {
+      mounted = false;
+    };
   }, [location]);
   return (
-    <div className="detail__template">
+    <div className="content_container">
       <DetailComponent post={article} loaded={loaded} />
       <CommentTemplate />
     </div>
