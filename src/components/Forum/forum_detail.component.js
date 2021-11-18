f708bcc0b3327ec959b8efda0fc797572f4dbc72
Junhyuck Ko <mrbartrns@naver.com>
2021-11-18 15:19:18 +0900

    Create forum-detail-component

--- src/components/Forum/forum_detail.component.js
+++ src/components/Forum/forum_detail.component.js
@@ -1,7 +1,54 @@
 import React from "react";
 
+import "../../css/component/forum.component.scss";
+
 function ForumDetailComponent(props) {
-  return <div></div>;
+  return (
+    <article className="board_article">
+      <div className="article_wrapper">
+        <div className="article_head">
+          <div className="title_row">
+            <div className="title">
+              <span className="title_name">{props.article.title}</span>
+            </div>
+          </div>
+          <div className="info_row">
+            <div className="member_info">
+              <span className="user_info">{props.article.author.username}</span>
+            </div>
+            <div className="article_info">
+              <span className="article_info__head head">추천</span>
+              <span className="article_info__body">
+                {props.article.like_count}
+              </span>
+              <span className="sep"></span>
+              <span className="article_info__head head">비추천</span>
+              <span className="article_info__body">
+                {props.article.dislike_count}
+              </span>
+              <span className="sep"></span>
+              <span className="article_info__head head">조회수</span>
+              <span className="article_info__body">{props.article.hit}</span>
+              <span className="sep"></span>
+              <span className="article_info__head head">작성일</span>
+              <span className="article_info__body">
+                {new Date(props.article.dt_created).toLocaleString()}
+              </span>
+              <span className="sep"></span>
+              <span className="article_info__head head">수정일</span>
+              <span className="article_info__body">
+                {new Date(props.article.dt_modified).toLocaleString()}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="article_body">
+          <div className="article_link"></div>
+          <div className="article_content fr_view">{props.article.content}</div>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default ForumDetailComponent;
--- src/templates/forum_detail.template.js
+++ src/templates/forum_detail.template.js
@@ -1,23 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import ForumDetailComponent from "../components/Forum/forum_detail.component";
 import userService from "../services/user.service";
 
 function ForumDetailTemplate(props) {
   const location = useLocation();
   console.log(location);
-  const [article, setArticle] = useState(null);
+  const [article, setArticle] = useState({});
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     let mounted = true;
     if (mounted) {
-      userService.getForumArticle(location.pathname, {}).then((response) => {
-        console.log(response.data);
-      });
+      setLoaded(false);
+      userService
+        .getForumArticle(location.pathname, {})
+        .then((response) => {
+          setArticle(response.data);
+          setLoaded(true);
+        })
+        .catch((error) => {
+          setLoaded(true);
+          console.error(error);
+        });
     }
     return () => {
       mounted = false;
     };
   }, [location]);
-  return <div></div>;
+  return loaded && <ForumDetailComponent article={article} />;
 }
 
 export default ForumDetailTemplate;
