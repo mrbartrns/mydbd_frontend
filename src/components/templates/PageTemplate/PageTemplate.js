cf7bb042ad8695a963ef2ec14ddec34b9030b620
Junhyuck Ko <mrbartrns@naver.com>
2021-12-17 18:42:11 +0900

    work: create page template

---
+++ src/components/templates/PageTemplate/PageTemplate.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function PageTemplate({ children }) {
+  return (
+    <Container md>
+      <main></main>
+    </Container>
+  );
+}
+
+export default PageTemplate;
