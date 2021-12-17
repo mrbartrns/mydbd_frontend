8577bc8de382912cdf35c2f125f3dc618bf279d1
Junhyuck Ko <mrbartrns@naver.com>
2021-12-17 18:42:24 +0900

    work: with page template

--- src/components/organisms/LoginModal/LoginModal.js
+++ src/components/organisms/LoginModal/LoginModal.js
@@ -1,21 +1,15 @@
 import React from "react";
 import { Modal } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import classNames from "classnames/bind";
 import style from "./LoginModal.scss";
 import LoginForm from "../../molecules/LoginForm";
 
 const cx = classNames.bind(style);
 
-function LoginModal({
-  username,
-  password,
-  error,
-  onSubmit,
-  className,
-  ...rest
-}) {
+function LoginModal({ username, password, error, onSubmit, className }) {
   return (
-    <Modal.Dialog className={cx("login-modal", {}, className)} {...rest}>
+    <Modal.Dialog className={cx("login-modal", {}, className)}>
       <Modal.Header>
         <Modal.Title>로그인</Modal.Title>
       </Modal.Header>
@@ -27,6 +21,9 @@ function LoginModal({
           onSubmit={onSubmit}
         />
       </Modal.Body>
+      <Modal.Footer className={cx("login-modal-footer")}>
+        <Link to="/signup">아직 회원가입을 하지 않았다면, 가입하러 가기</Link>
+      </Modal.Footer>
     </Modal.Dialog>
   );
 }
