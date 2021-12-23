39eb9d19f682c1ff42a486c38c4cafa57e04bddb
Junhyuck Ko <mrbartrns@naver.com>
2021-12-23 12:48:36 +0900

    fix: connect redux to navbar right

--- src/components/molecules/NavbarRight/NavbarRight.js
+++ src/components/molecules/NavbarRight/NavbarRight.js
@@ -1,14 +1,27 @@
 import React from "react";
 import NavbarWrapper from "../../atoms/NavbarWrapper";
 import { Nav } from "react-bootstrap";
+import { connect } from "react-redux";
 
-function NavbarRight() {
+function NavbarRight({ isLoggedIn, user, dispatch, ...rest }) {
+  console.log(isLoggedIn, user);
   return (
     <NavbarWrapper>
-      <Nav.Link href="/login">로그인</Nav.Link>
-      <Nav.Link href="/signup">회원가입</Nav.Link>
+      {!isLoggedIn ? (
+        <>
+          <Nav.Link href="/login">로그인</Nav.Link>
+          <Nav.Link href="/signup">회원가입</Nav.Link>
+        </>
+      ) : (
+        <Nav.Link>로그아웃</Nav.Link>
+      )}
     </NavbarWrapper>
   );
 }
 
-export default NavbarRight;
+function mapStateToProps(state) {
+  const { isLoggedIn, user } = state.authReducer;
+  return { isLoggedIn, user };
+}
+
+export default connect(mapStateToProps)(NavbarRight);
