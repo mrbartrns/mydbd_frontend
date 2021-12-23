c5aeb8d2258983a57261e7572c26930c0172c3c8
Junhyuck Ko <mrbartrns@naver.com>
2021-12-23 18:25:33 +0900

    work: remove unused imports

--- src/components/templates/PageTemplate/PageTemplate.js
+++ src/components/templates/PageTemplate/PageTemplate.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import classNames from "classnames/bind";
 import style from "./PageTemplate.scss";
-import Navbar from "../../organisms/Navbar";
 import NavbarContainer from "../../../containers/NavbarConatiner/NavbarContainer";
 
 const cx = classNames.bind(style);
