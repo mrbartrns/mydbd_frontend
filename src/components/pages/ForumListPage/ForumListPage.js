48db9bf327a577b3b2bb2bff81f3bb039fa7ffbc
Junhyuck Ko <mrbartrns@naver.com>
2021-12-29 23:41:04 +0900

    epic: apply helmet

--- src/components/pages/ForumListPage/ForumListPage.js
+++ src/components/pages/ForumListPage/ForumListPage.js
@@ -2,11 +2,16 @@ import React from "react";
 import ForumListTemplate from "../../templates/ForumListTemplate";
 import PageTemplate from "../../templates/PageTemplate";
 import ForumListContainer from "../../../containers/ForumListContainer";
+import { Helmet } from "react-helmet";
 
 function ForumListPage() {
   return (
     <PageTemplate>
       {/** Helmet */}
+      <Helmet>
+        <title>포럼-MYDBD</title>
+        <meta property="og:title" content="포럼-MYDBD" />
+      </Helmet>
       <ForumListTemplate>
         <ForumListContainer />
       </ForumListTemplate>
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Helmet } from "react-helmet";
 import PageTemplate from "../../templates/PageTemplate";
 
 function HomePage() {
   return (
     <PageTemplate>
       {/** Helmet position */}
+      <Helmet>
+        <title>홈-MYDBD</title>
+        <meta property="og:title" content="홈-MYDBD" />
+      </Helmet>
       <h1>HomePage</h1>
     </PageTemplate>
   );
